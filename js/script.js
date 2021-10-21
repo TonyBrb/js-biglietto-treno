14b43eaa69d7f0af9445e68d87937a2fb3437dbc
TonyBrb <anthonybarbagallo95@gmail.com>
2021-10-21 16:27:22 +0200

    esercizio con bonus

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
-const km = prompt('Quanti km vuoi percorrere?');
-const age = prompt('Inserisci la tua età');
+let km = prompt('Quanti km vuoi percorrere?');
+let age = prompt('Inserisci la tua età');
 let price = 0;
+let errorMsg = '';
+let codeSconto = '';
 
+if(isNaN(km)){
+  km = false;
+  errorMsg = 'Inserire un numero di km valido';
+}
+
+if(isNaN(age)){
+  age = false;
+  errorMsg = 'Inserire un numero di anni valido';
+}
+
+if(age <= 20){
+  codeSconto = prompt('inserisci il codice sconto SCONTO20 per avere un ulteriore 20% di sconto sul biglietto')
+  price = (km * 0.21) - ((km * 0.21)*20)/100;
+}
+
+if(codeSconto !== 'SCONTO20'){
+  codeSconto = false;
+  errorMsg = 'Inserire un codice sconto valido';
+}
+
+console.log(errorMsg);
 console.log(km);
 console.log(age);
 
 
 if(age < 18){
-  price = (km * 0.21) - ((km * 0.21)*20)/100;
+  price = (km * 0.21) - ((km * 0.21)*40)/100;
+}
+
+if(age > 20 && age <=65){
+  price = km * 0.21;
 }
 
 if(age > 65){
